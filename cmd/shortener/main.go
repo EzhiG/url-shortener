9d8afa337c35ef6080f047fcca575d4fafde43b1
Vorzhev Anton <metalezhig@gmail.com>
2026-07-24 14:42:04 +0300

    add chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,18 +6,20 @@ import (
 	"github.com/EzhiG/url-shortener/internal/handler"
 	"github.com/EzhiG/url-shortener/internal/repository"
 	"github.com/EzhiG/url-shortener/internal/shortener"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	storage := repository.NewMapStorage()
-	serv := shortener.NewService(storage)
-	h := handler.NewHandler(serv)
+	service := shortener.NewService(storage)
+	h := handler.NewHandler(service)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("POST /", h.PostShortenUrl)
-	mux.HandleFunc("GET /{id}", h.GetShortenUrl)
+	router := chi.NewRouter()
+	router.Post("/", h.PostShortenUrl)
+	router.Get("/{id}", h.GetShortenUrl)
 
-	err := http.ListenAndServe(`:8080`, mux)
+	err := http.ListenAndServe(`:8080`, router)
+
 	if err != nil {
 		panic(err)
 	}
